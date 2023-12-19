1799b160914475d1f4918a7512cd7f97f7851aa0
JinsuRhee <jinsu.rhee@gmail.com>
2023-12-19 17:56:59 +0900

    (JS-Perform) off scaling bug in FOFSearchBallCriterion2

--- src/KDTree/KDSplitNode.cxx
+++ src/KDTree/KDSplitNode.cxx
@@ -865,9 +865,9 @@ namespace NBody
     void SplitNode::FOFSearchBall(Double_t rd, Double_t fdist2, Int_t iGroup, Int_t nActive, Particle *bucket, Int_t *Group, Int_tree_t *Len, Int_tree_t *Head, Int_tree_t *Tail, Int_tree_t *Next, short *BucketFlag, Int_tree_t *Fifo, Int_t &iTail, Double_t* off, Int_t target)
     {
 	if(BucketFlag[nid]) return;
-	int flat=1;
+	int flag=1;
 
-	flat=0;
+	flag=0;
         Double_t old_off = off[cut_dim];
         Double_t new_off = bucket[target].GetPhase(cut_dim) - cut_val;
         if (new_off < 0)
@@ -908,7 +908,7 @@ namespace NBody
 
 	int flag=1;
 
-	flag=0
+	flag=0;
 
         Double_t old_off = off[cut_dim];
         Double_t new_off = bucket[target].GetPhase(cut_dim) - cut_val;
